4100a07426f27fb98965b9d1a05f93aa5d920b66
Becca Agunga <beccaagunga@gmail.com>
2020-05-06 22:25:34 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <main id="app" @mousemove="mousemove">
+    <div class="logo">
+      <img src="./assets/orange.png" alt="Nike" />
+    </div>
+    <div class="container">
+      <section class="products">
+        <Product
+          v-for="product in products"
+          :key="product.color"
+          :product="product"
+        />
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import Product from "./components/Product";
+// import { gsap } from "gsap";
+
+export default {
+  name: "App",
+  components: {
+    Product
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: "OFF-WHITE x Air Max 90 'Black'",
+          color: "black",
+          bgtext: "NIKE",
+          desc:
+            "This AM 90 is sporting a black upper, white Nike “Swoosh”, black midsole, and a black sole. These sneakers released in January 2019 and retailed for $160.",
+          src: require("./assets/am-blk.png")
+        },
+        {
+          title: "OFF-WHITE x Air Max 90 'Desert Ore'",
+          color: "desert",
+          bgtext: "AIR",
+          desc:
+            "This AM 90 comes with a beige upper, mango orange Nike “Swoosh”, beige midsole, and beige sole. These sneakers released in January 2019 and retailed for $160.",
+          src: require("./assets/am-ds.png")
+        },
+        {
+          title: "OFF-WHITE x Air Max 90 'The Ten'",
+          color: "ice",
+          bgtext: "MAX",
+          desc:
+            "This AM 90 combines a white leather upper with ice blue foam and grey suede overlays, a stitched reflective white Nike Swoosh, signature orange tag, and a grey suede heel patch. These sneakers released in January 2019 and retailed for $160.",
+          src: require("./assets/am-wht.png")
+        }
+      ]
+    };
+  },
+  methods: {
+    mousemove(e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let products = document.querySelectorAll(".products, .product");
+      for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+
+        let product_image = product.querySelector(".product-image-wrap");
+
+        let img_x = mouseX - this.coords(product_image).x;
+        let img_y = mouseY - this.coords(product_image).y;
+        product_image.style.transform = `translateY(-${img_y /
+          100}px) translateX(-${img_x / 100}px) translateZ(100px)`;
+
+        let bgtext = product.querySelector(".bg-text");
+        let bg_x = mouseX - this.coords(bgtext).x;
+        // let bg_y = mouseY - this.coords(bgtext).y;
+        bgtext.style.transform = `translateX(${bg_x / 70}px)`;
+      }
+    },
+    coords(el) {
+      let coords = el.getBoundingClientRect();
+
+      return {
+        x: coords.left / 2,
+        y: coords.top / 2
+      };
+    }
+  }
+};
+</script>
+
+<style>
+.overlay {
+  z-index: 1;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background: #fff;
+  top: 0;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+.container {
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #eee;
+}
+
+.products {
+  display: flex;
+  max-width: 1280px;
+  padding: 25px;
+}
+
+.logo {
+  height: 100px;
+  display: flex;
+  background-color: #eee;
+}
+
+.logo img {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="product">
+    <div :class="'product-inner ' + product.color">
+      <div class="product-text-wrap">
+        <h2 class="bg-text">{{ product.bgtext }}</h2>
+      </div>
+      <div class="product-image-wrap">
+        <img class="image" :src="product.src" />
+      </div>
+      <div class="product-detail">
+        <h2>{{ product.title }}</h2>
+        <p class="product-desc">{{ product.desc }}</p>
+        <div class="inner-desc">
+          <p class="price">$160</p>
+          <button type="submit">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product",
+  props: ["product"]
+};
+</script>
+
+<style>
+.product {
+  flex: 1 1 33.333%;
+  width: 100%;
+  padding: 25px;
+}
+
+.product-inner {
+  position: relative;
+  padding: 25px;
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  perspective: 1000px;
+}
+
+.product-inner.black {
+  background-image: linear-gradient(to bottom right, #f4f4f7, #2b2c30);
+}
+.product-inner.desert {
+  background-image: linear-gradient(to bottom left, #dcdae8, #f27b54 50%);
+}
+.product-inner.ice {
+  background-image: linear-gradient(to bottom right, #dfdee3, #c5bbb5 70%);
+}
+
+.product-text-wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 0;
+  overflow: hidden;
+  perspective: 1000px;
+}
+
+.product-text-wrap h2 {
+  color: #000;
+  font-size: 128px;
+  font-weight: 900;
+  opacity: 0.2;
+  transform-origin: center;
+  text-align: center;
+}
+
+.product-image-wrap .image {
+  width: 100%;
+  filter: drop-shadow(0px 0px 12px rgba(0, 0, 0 0.25));
+}
+
+.product-image-wrap {
+  position: relative;
+  z-index: 1;
+  transform-origin: center;
+  padding: 10px;
+}
+
+.product-detail {
+  background-color: #fff;
+  padding: 25px;
+  margin: 0px -25px -25px;
+}
+
+.product-detail h2 {
+  font-size: 24px;
+  font-weight: 700;
+  color: #676767;
+  margin-bottom: 15px;
+}
+
+.product-desc {
+  font-size: 14px;
+  line-height: 1.5;
+  font-weight: 300;
+  color: #676767;
+  height: 120px;
+}
+
+.inner-desc {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.price {
+  font-weight: 600;
+}
+button {
+  cursor: pointer;
+  margin-left: auto;
+  padding: 8px 24px;
+  text-decoration: none;
+  color: linear-gradient(to bottom right, #dfdee3, #c5bbb5 70%);
+  font-size: 18px;
+  border-radius: 20px;
+  transition: 0.4s linear;
+  color: #f17850;
+  border: 2px solid;
+}
+
+button:hover {
+  color: red;
+}
+</style>
